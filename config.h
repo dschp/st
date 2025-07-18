e267e7f066c96f43f4aa4df217d30ab253eaba2e
Eyr <dschp@dzy.me>
2025-07-18 19:12:55 +0000

    Update config

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "monospace:pixelsize=19:antialias=true:autohint=true";
+static char *font = "JetBrains Mono:pixelsize=17:antialias=true:autohint=true";
 static int borderpx = 0;
 
 /*
